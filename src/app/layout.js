69c3826c4dfa907a4ebc518a454abebdf28561e7
S Somsekhar <ssomsekhar27@gmail.com>
2025-06-14 11:31:00 +0530

    added chat widget

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,6 @@ export const metadata = {
   },
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -47,8 +46,24 @@ export default function RootLayout({ children }) {
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
+
+        {/* Tawk.to Live Chat Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/684d0d796b65fa190ea6e6e2/1itmfp8um';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
 }
-      
