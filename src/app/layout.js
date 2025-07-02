4c4578be6c9cbd263e08496460ac5ee652da6480
S Somsekhar <ssomsekhar27@gmail.com>
2025-07-02 22:23:47 +0530

    domain redirect

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,7 +44,9 @@ export default function RootLayout({ children }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-G5BJJRCYVL');
+              gtag('config', 'G-G5BJJRCYVL', {
+                cookie_domain: 'clientnesthq.com'
+              });
             `,
           }}
         />
